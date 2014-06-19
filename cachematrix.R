b4173d1eecb38d3db650447ede1106d458f6f78c
hcarlsso <hakan.carlsson@gmail.com>
2014-06-19 20:17:03 +0200

    First implementation, functionality works, but is not faster

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,55 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    ## Function to create the cache for the matrices inverse
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    setinv <- function(inverse) m <<- inverse
+    getinv <- function() m
+    list(set = set, get = get,
+         setinv = setinv,
+         getinv = getinv)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    m <- x$getinv()
+    if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
+    }
+    data <- x$get()
+    # When solved is called without any second arguments, 
+    # it calculates the inverse
+    m <- solve(data, ...)
+    x$setinv(m)
+    m
 }
+
+native_inverse  <- function(iter,matrix){
+    
+    for(i in seq(0,iter)){
+            
+        solve(matrix) 
+    }
+}
+
+# Test functionality
+# Use random numbers, infinite small change the matrix is not invertable
+matrix_to_solve  <- matrix(rnorm(100),10,10)
+matrix_solved_native  <- solve(matrix_to_solve)
+matrix_solved_cache  <- cacheSolve(makeCacheMatrix(matrix_to_solve))
+difference  <- matrix_solved_native - matrix_solved_cache
+difference
+# solve(matrix)
+iterations  <- 1E5
+
+system.time(replicate( iterations, cacheSolve(makeCacheMatrix(matrix_to_solve))))
+system.time(native_inverse(iterations,matrix_to_solve))
